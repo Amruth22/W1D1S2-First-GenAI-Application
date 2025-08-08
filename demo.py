5fdf4811e7db55d4dbd9133eb907f61382c1e8e2
Amruth22 <89969774+Amruth22@users.noreply.github.com>
2025-08-08 15:03:21 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -24,7 +24,7 @@ def generate():
         types.Content(
             role="user",
             parts=[
-                types.Part.from_text(text="""INSERT_INPUT_HERE"""),
+                types.Part.from_text(text="""Explain quantum computing in simple terms"""),
             ],
         ),
     ]
